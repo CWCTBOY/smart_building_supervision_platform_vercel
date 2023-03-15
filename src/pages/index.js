dd9a8420200b37197f62cee7d4093950d27af8d4
CWCTBOY <qkrdlswo98@gmail.com>
2023-03-15 17:49:43 +0000

    CI/CD - revise workflow yma filename

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,10 @@ const MainContainer = styled.div`
 const Main = () => {
   return (
     <MainContainer>
-      <h1>CI/CD Final Test</h1>
+      <h1>
+        Welcome to <br />
+        Creative React App
+      </h1>
     </MainContainer>
   );
 };
